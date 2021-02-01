9fcfb88dfcf8997c845191162ddfc06ba8319bfb
luvrace <rambn1993@knou.ac.kr>
2021-02-01 20:49:45 +0900

    add greeting

--- clocks.js
+++ clocks.js
@@ -4,13 +4,17 @@ function getTime() {
   const now = new Date();
   const hours = now.getHours();
   const minutes = now.getMinutes();
-  const seconds = now.getSeconds();
-  clockTitle.innerHTML = time;
+  const time = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }`;
+  clock.innerHTML = time;
   return;
 }
 
 function init() {
   getTime();
+  setInterval(getTime, 1000);
+  return;
 }
 
 init();
