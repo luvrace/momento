fd8dfd5b78b80e4694f5c2f9174a9e8cf7752bda
luvrace <rambn1993@knou.ac.kr>
2021-02-11 10:34:07 +0900

    UPDATE SAVE LOCAL STORAGE

--- todo.js
+++ todo.js
@@ -4,20 +4,45 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_LS,JSON.stringify(toDos));
+}
+
 function paintToDo(text){
-    console.log(text);
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    delBtn.value = "X";
+    const span = document.createElement("span");
+    const newId = toDos.length +1;
+    span.innerText = text;
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    li.id = newId;
+    toDoList.appendChild(li);
+    const toDoObj = {
+        text: text,
+        id:newId,
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(event){
     event.preventDefault();
     const currentValue = toDoInput.value;
     paintToDo(currentValue);
+    toDoInput.value ="";
 }
 
 function loadToDos(){
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null){
-        
+    loadToDos = localStorage.getItem(TODOS_LS);
+    if(loadToDos !== null){
+        const parsedToDos = JSON.parse(loadToDos);
+        parsedToDos.forEach(toDo => {
+            paintToDo(toDo.text);
+        });
     }
 }
 function init(){
